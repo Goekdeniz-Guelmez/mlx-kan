95e172d84cb0a72c50e50685146755227c27b490
Goekdeniz-Guelmez <Gulmezdeniz1999@gmail.com>
2024-07-24 21:26:20 +0200

    initial commit

---
+++ kan/__init__.py
@@ -0,0 +1,4 @@
+from .KAN import KANLinear, KAN
+from .KANConvolution import KAN_Convolutional_Layer, KAN_Convolution
+
+__all__ = ["KANLinear", "KAN", "KAN_Convolutional_Layer", "KAN_Convolution"]
---
+++ setup.py
@@ -0,0 +1,35 @@
+import sys
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+# Get the project root directory
+root_dir = Path(__file__).parent
+
+# Add the package directory to the Python path
+package_dir = root_dir / "kan"
+sys.path.append(str(package_dir))
+
+# Read the requirements from the requirements.txt file
+requirements_path = root_dir / "requirements.txt"
+with open(requirements_path) as fid:
+    requirements = [l.strip() for l in fid.readlines()]
+
+# Import the version from the package
+from version import __version__
+
+# Setup configuration
+setup(
+    name="mlx-kan",
+    version=__version__,
+    description="KAN: Kolmogorov–Arnold Networks on Apple silicon with MLX.",
+    long_description=open(root_dir / "README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author_email="goekdenizguelmez@gmail.com",
+    author="Gökdeniz Gülmez",
+    url="https://github.com/Goekdeniz-Guelmez/mlx-kan",
+    license="Apache2.0",
+    install_requires=requirements,
+    packages=find_packages(where=root_dir),
+    python_requires=">=3.8"
+)

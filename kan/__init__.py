da303bb22011db41bab85bc4c3f67780d5ca31cf
Goekdeniz-Guelmez <Gulmezdeniz1999@gmail.com>
2024-07-27 21:41:31 +0200

    update

--- kan/__init__.py
+++ kan/__init__.py
@@ -5,7 +5,7 @@ def __getattr__(name):
         from .kan import KANLinear, KAN
         return locals()[name]
     elif name in ("KAN_Convolutional_Layer", "KAN_Convolution"):
-        from .convolution.kanConvolution import KAN_Convolutional_Layer, KAN_Convolution
+        from .kan_convolution.kanConvolution import KAN_Convolutional_Layer, KAN_Convolution
         return locals()[name]
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
 
--- quick_scripts/quick_train.py
+++ quick_scripts/quick_train.py
@@ -7,7 +7,7 @@ import mlx.optimizers as optim
 import numpy as np
 import quick_scripts.mnist as mnist
 
-from kan import KAN, KAN_Convolutional_Layer
+from kan import KAN
 
 from quick_scripts.utils import print_trainable_parameters, save_model, save_config, create_save_directory
 

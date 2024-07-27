484b953ef19d4214df5e025e71f3e24f2ab51609
Goekdeniz-Guelmez <Gulmezdeniz1999@gmail.com>
2024-07-27 17:29:53 +0200

    last try

--- kan/__init__.py
+++ kan/__init__.py
@@ -1,4 +1,12 @@
-from .kan import KANLinear, KAN
-from .convolution.kanConvolution import KAN_Convolutional_Layer, KAN_Convolution
+from .version import __version__
+
+def __getattr__(name):
+    if name in ("KANLinear", "KAN"):
+        from .kan import KANLinear, KAN
+        return locals()[name]
+    elif name in ("KAN_Convolutional_Layer", "KAN_Convolution"):
+        from .convolution.kanConvolution import KAN_Convolutional_Layer, KAN_Convolution
+        return locals()[name]
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
 
 __all__ = ["KANLinear", "KAN", "KAN_Convolutional_Layer", "KAN_Convolution"]
